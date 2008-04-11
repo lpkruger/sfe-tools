fa40c3898a091d8879b9e4063fc5e540a8a71457
louis <louis@16ffc253-0420-0410-a4db-cfc06cdcecfe>
2008-04-11 23:52:16 +0000

    Can do additive and multiplicative
    git-svn-id: svn://localhost/repos@92 16ffc253-0420-0410-a4db-cfc06cdcecfe

--- sfe/prolog/homomorphic.pl
+++ sfe/prolog/homomorphic.pl
@@ -1,4 +1,4 @@
-:- dynamic(has/2).
+:- dynamic(hasLearned/2).
 :- dynamic(canLearn/2).
 
 isParty(alice).
@@ -10,14 +10,15 @@ belongs(n, bob).
 belongs(r1, alice).
 belongs(r2, bob).
 
-canLearn(_,_) :- fail.
-canLearn(mul(add(x,y),r1), alice).
-canLearn(add(x,y), alice).
-canLearn(add(y,r1), bob).
-canLearn(add(x,add(y,r1)), alice).
+%canLearn(_,_) :- fail.
+%canLearn(mul(add(x,y),r1), alice).
+%canLearn(add(x,y), alice).
+%canLearn(mul(x,y), alice).
+%canLearn(add(y,r1), bob).
+%canLearn(add(x,add(y,r1)), alice).
 
-%test :- derive(add(x,y), alice).
-test :- derive(add(x,add(y,r1)), alice).
+test :- derive(add(x,y), alice).
+%test :- derive(add(x,add(y,r1)), alice).
 
 
 
@@ -27,21 +28,95 @@ test :- derive(add(x,add(y,r1)), alice).
 
 init :- b_setval(out, []), nb_setval(out2, []).
 appendGlobal(X) :-
-  b_getval(out, R), b_setval(out, [X | R]),
-  b_getval(out, Q), nb_setval(out2, Q).
-output :- writeln('-------\n'), nb_getval(out2, R), output(R).
+	b_getval(out, R), 
+	(memberchk(X, R) -> !, true ; 
+	b_setval(out, [X | R]), b_getval(out, Q), nb_setval(out2, Q)).
 
 
-output([]).
-output([sendTo(X, A, B) | R]) :- output(R),
-  write(A), write(': sendTo('), write(B), write(', '), write(X), write(')\n').
+writeCalcTerm(addEnc(enc(X,EE),enc(Y,EE))) :-
+	Z = enc(add(X,Y),EE), writeProtoTerm(Z), write(' = addEnc('),
+	Args = [ enc(X,EE), enc(Y,EE) ], writeCalcTermList(Args), writeln(');').
+writeCalcTerm(subEnc(enc(X,EE),enc(Y,EE))) :-
+	Z = enc(sub(X,Y),EE), writeProtoTerm(Z), write(' = subEnc('),
+	Args = [ enc(X,EE), enc(Y,EE) ], writeCalcTermList(Args), writeln(');').
+writeCalcTerm(mulEnc(enc(X,EE),enc(Y,EE))) :-
+	Z = enc(mul(X,Y),EE), writeProtoTerm(Z), write(' = mulEnc('),
+	Args = [ enc(X,EE), enc(Y,EE) ], writeCalcTermList(Args), writeln(');').
+writeCalcTerm(mulEnc(enc(X,EE),Y)) :- fail.    % -----> TODO
+writeCalcTerm(reEnc(enc(X, E2), E1)) :-
+	Z = enc(X,E2), writeProtoTerm(Z), write(' = reEnc('),
+	Args = [ enc(X,E1), E1, E2 ], writeCalcTermList(Args), writeln(');').
+writeCalcTerm(dec(enc(X,EE),DD)) :- 
+	writeProtoTerm(X), write(' = dec('), 
+	Args = [ enc(X,EE), DD], writeCalcTermList(Args), writeln(');').
+
+writeCalcTerm(X) :- X =.. [Func | Args], writeProtoTerm(X), write(' = '), write(Func), write('('),
+	writeCalcTermList(Args), writeln(');').
+
+writeCalcTermList([]).
+writeCalcTermList([H]) :- writeProtoTerm(H).
+writeCalcTermList([H | R]) :-  writeProtoTerm(H), write(', '), writeCalcTermList(R).
+
+
+writeProtoTerm(X) :- atomic(X), write(X).
+writeProtoTerm(X) :- X =.. [Func | Args], writeProtoTermList([Func | Args]).
+writeProtoTerm(X) :- nl, write('UNKNOWN: '), writeln(X).
+
+writeProtoTermList([]).
+writeProtoTermList([H | R]) :- writeProtoTerm(H), write('_'), writeProtoTermList(R).
+
+writeProtoHeader(W) :- write('function '), write(W), write('() '), writeln('{').
+writeProtoFooter(_) :- writeln('}').
+
+outProto(W) :- !, writeln('-------\n'), nb_getval(out2, R), 
+	writeProtoHeader(W), outProto(R, W), writeProtoFooter(W), !.
+
+outProto([], _).
+outProto([sendTo(X, W, B) | R], W) :- outProto(R, W),
+	write('send("'), write(B), write('", '),
+	writeProtoTerm(X), writeln(');').
+outProto([sendTo(X, A, W) | R], W) :- outProto(R, W),
+	writeProtoTerm(X), write(' = '),
+	write('recv("'), write(A), writeln('");').
+
+outProto([calc(X, W) | R], W) :- outProto(R, W),
+	writeCalcTerm(X), nl.
+
+outProto([_ | R], W) :- outProto(R, W).
+outProto(X, _) :- nl, write('UNKNOWN: '), writeln(X).
+
+
+
+output(all) :- !, writeln('-------\n'), nb_getval(out2, R), output(R), !.
+output(W) :- isParty(W), !, writeln('-------\n'), nb_getval(out2, R), output(R, W), !.
+	
+
+output([], _).
+output([sendTo(X, W, B) | R], W) :- output(R, W),
+	write(W), write(': sendTo('), write(B), write(', '), write(X), write(')\n').
+output([sendTo(X, A, W) | R], W) :- output(R, W),
+	write(W), write(': readFrom('), write(A), write(', '), write(X), write(')\n').
+	
+
+output([calc(X, W) | R], W) :- output(R, W),
+	write(W), write(': '), write(X), write('\n').
+output([sfe(X, W) | R], W) :- output(R, W),
+	write(W), write(': '), write('SFE('), write(X), write(')\n').
+output([_ | R], W) :- output(R, W).
 
 %special case for add:
 %output(calc(add(X,Y), W)) :-
 %  write(W), write(': '), write(X), write('+'), write(Y), write('\n').
-
+%%%%
+output([]).
+output([sendTo(X, A, B) | R]) :- output(R),
+  write(A), write(': sendTo('), write(B), write(', '), write(X),
+  write(')\n').
 output([calc(X, W) | R]) :- output(R),
   write(W), write(': '), write(X), write('\n').
+output([sfe(X, W) | R]) :- output(R),
+  write(W), write(': '), write(sfe(X)), write('\n').
+
 
 %sentTo(X, A, B) :- has(X,B).
 sentTo(X, A, B) :- isParty(A), isParty(B), A\==B, appendGlobal(sendTo(X, A, B)).
@@ -51,14 +126,22 @@ calc(sub(X,Y),W) :- isParty(W), appendGlobal(calc(sub(X,Y),W)).
 
 calc(X,W) :- isParty(W), appendGlobal(calc(X, W)).
 
-isntEnc(X) :- not(X = enc(_,_)).
+isntEnc(X) :- not(X = enc(_,_)), not(X = pubKey(_,_)).
+
+isEncMode(additive).
+isEncMode(multiplicative).
+
+isPubKey(EE) :- EE=pubKey(X, W), isParty(X), isEncMode(W).
+
+% reuse computations
+%has(X,W) :- hasLearned(X,W), !.
 
 % ground rules: people have their own keys and inputs and constants
 has(X,W) :- belongs(X,W).
 has(X,W) :- integer(X).
 has(privKey(A),A).
-has(pubKey(A),A).
-has(pubKey(A),W) :- sentTo(pubKey(A), A, W).
+has(pubKey(A,M),A) :- isPubKey(pubKey(A,M)).
+has(pubKey(A,M),W) :- isPubKey(pubKey(A,M)), sentTo(pubKey(A,M), A, W).
 
 % simple arithmetic
 has(add(X,Y),W) :- has(X, W), has(Y, W), calc(add(X,Y),W).
@@ -66,30 +149,55 @@ has(sub(X,Y),W) :- has(X, W), has(Y, W), calc(sub(X,Y),W).
 has(mul(X,Y),W) :- has(X, W), has(Y, W), calc(mul(X,Y),W).
 
 % can use public key to encrypt
-has(enc(X, E), W) :- has(pubKey(E), W), has(X, W), calc(enc(X,E), W).
+has(enc(X, EE), W) :- isPubKey(EE), has(EE, W), has(X, W), calc(enc(X,EE), W).
 
 % can send an encrypted value to someone if it's not their key
-has(enc(X, E), W) :- W\==E, has(enc(X, E), V), sentTo(enc(X, E), V, W).
+has(enc(X, EE), W) :- isPubKey(EE), EE=pubKey(E, M), W\==E, has(enc(X, EE), V), sentTo(enc(X, EE), V, W).
 
 % can send an encrypted value to someone with their own key if they
 % are allowed to learn the value
-has(enc(X, E), W) :- canLearn(X, W), has(enc(X, E), V), writeln('*'), sentTo(enc(X, E), V, W).
+%
+has(X, W) :- canLearn(X, W), has(X, V), writeln('*'), sentTo(X, V, W).
+
+has(enc(X, EE), W) :- isPubKey(EE), EE=pubKey(E, _), W==E, canLearn(X, W), has(enc(X, EE), V), writeln('*'), sentTo(enc(X, EE), V, W).
 
 % can decrypt an encrypted value if they have the private key
 % D=W assumes no sharing of priv keys
-has(X,W) :- D=W, isntEnc(X), has(enc(X,D), W), calc(dec(enc(X,D),D),W).
+has(X,W) :- D=W, EE=pubKey(D, M), isPubKey(EE), isntEnc(X), has(enc(X,EE), W), 
+	DD=privKey(D, M), calc(dec(enc(X,EE),DD),W).
 
+%%% additive homomorphic rules
 % can add two values encrypted with E's key
-has(enc(add(X,Y),E), W) :- has(enc(X,E),W), has(enc(Y,E),W),
-  calc(addEnc(enc(X,E),enc(Y,E)), W).  
-has(enc(sub(X,Y),E), W) :- has(enc(X,E),W), has(enc(Y,E),W),
-  calc(subEnc(enc(X,E),enc(Y,E)), W).  
+has(enc(add(X,Y),EE), W) :- EE=pubKey(_,additive), isPubKey(EE),
+	has(enc(X,EE),W), has(enc(Y,EE),W),
+  	calc(addEnc(enc(X,EE),enc(Y,EE)), W).  
+has(enc(sub(X,Y),EE), W) :- EE=pubKey(_,additive), isPubKey(EE),
+	has(enc(X,EE),W), has(enc(Y,EE),W),
+  	calc(subEnc(enc(X,EE),enc(Y,EE)), W).  
 
 % can multiply an encrypted value by a non-encrypted value
-has(enc(mul(X,Y),E), W) :- has(enc(X,E),W), has(Y,W),
-  calc(mulEnc(enc(X,E),Y), W).
-has(enc(mul(Y,X),E), W) :- has(enc(X,E),W), has(Y,W),
-  calc(mulEnc(enc(X,E),Y), W).
+has(enc(mul(X,Y),EE), W) :- EE=pubKey(_,additive), isPubKey(EE),
+	has(enc(X,EE),W), has(Y,W), calc(mulEnc(enc(X,EE),Y), W).
+%has(enc(mul(Y,X),EE), W) :- EE=pubKey(_,additive), isPubKey(EE), 
+%	has(enc(X,EE),W), has(Y,W), calc(mulEnc(enc(X,EE),Y), W).
+
+has(enc(mul(Y,X),EE), W) :- has(enc(mul(X,Y), EE), W).
+	
+%%% multiplicative homomorphic rule
+has(enc(mul(X,Y),EE), W) :- EE=pubKey(_,multiplicative), isPubKey(EE),
+	has(enc(X,EE),W), has(enc(Y,EE),W),
+  	calc(mulEnc(enc(X,EE),enc(Y,EE)), W).  
+has(enc(exp(X,Y),EE), W) :- EE=pubKey(_,multiplicative), isPubKey(EE),
+	has(enc(X,EE),W), has(Y,W), calc(expEnc(enc(X,EE),Y), W).
+
+% catch all using reencryption
+%has(enc(add(x,y), pubKey(_, multiplicative)), _) :- trace, fail.
+has(enc(X, E2), W) :- E2=pubKey(E, M2), isPubKey(E2),
+	E1=pubKey(E, M1), isPubKey(E1), M1\=M2, has(enc(X, E1), W),
+	has(E2, W), has(E1, W),
+	calc(reEnc(enc(X, E2), E1), W).
+% catch all using SFE
+%has(X,W) :- canLearn(X,W), appendGlobal(sfe(X,W)), true.
 
 % For YAP
 %call_with_depth_limit(A,B,_) :- depth_bound_call(A,B).
@@ -100,9 +208,28 @@ derive1(X, W, L) :- call_with_depth_limit(has(X, W), L, Z),
 derive(X, W) :- derive0(X, W, 1) , !.
 */
 
-derive(X, W) :- init, clause_tree(has(X,W), []), output.
+:- dynamic shownProto/2.
+seenProto(X) :- nb_getval(out2, R), shownProto(X, R).
+assertProto(X) :- nb_getval(out2, R), asserta(shownProto(X, R)).
+derive(X, W) :- retractall(hasLearned(_,_)), retractall(shownProto(X, _)), 
+	affirm(canLearn(X,W)), init, clause_tree(has(X,W), []), 
+	not(seenProto(X)), output(all), output(alice), output(bob), assertProto(X).
 
+derive0(X, W, Proto) :- derive(X,W), nb_getval(out2, Proto).
+derive(X, W, Set) :- setof(Proto, derive0(X, W, Proto), Set).
 
+deriveBest(X, W) :- derive(X, W, Set), shortest(Set, R), nb_setval(out2, R), 
+	output(all), outProto(alice), outProto(bob).
+
+clear :- retractall(canLearn(_,_)), retractall(shownProto(_,_)).
+
+%% LL is a list of lists, L is the shortest list
+shortest(LLL, L) :- LLL = [L0 | LL], shortest0(LL, L0, L).
+shortest0([], L, L).
+shortest0([ L0 | LL ], L, LOut) :- length(L0, N0), length(L, N1), 
+	(N0<N1 -> shortest0(LL, L0, LOut) ; shortest0(LL, L, LOut)).
+
+%%%%%%%%%%% Meta-Interpreter %%%%%%%%%%%%%
 
 subst([],[]).
 subst([H|R], Z) :- var(H), !, subst(R,RR), Z=[H|RR].
@@ -141,12 +268,11 @@ clause_tree(G,Trail) :-
    clause(G,Body),
    %once(gsub(G, GG)),
    clause_tree(Body, [G|Trail]),
-   (G=has(X,W) -> affirm(G) ; true).
+   (G=has(X,W) -> affirm(hasLearned(X,W)) ; true).
 
 
-affirm(X).
-%affirm(X) :- clause(X,true), !.
-%affirm(X) :- asserta(X).
+affirm(X) :- clause(X, true), !.
+affirm(X) :- asserta(X).
 
 inner_loop_detect([H|R]) :- loop_detect(H,R).
 inner_loop_detect([_|R]) :- inner_loop_detect(R).
@@ -164,3 +290,9 @@ loop_detect(G,[_|R])  :- loop_detect(G,R).
 can_unify(G, G1) :- not(not(G=G1)).
 
 l(X,Y) :- not(not(X=Y)), belongs(X,Y).
+
+
+
+
+
+
