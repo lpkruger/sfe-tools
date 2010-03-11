0759bb13f4e2538c6a36602f874f10957710c1fd
louis <louis@16ffc253-0420-0410-a4db-cfc06cdcecfe>
2010-03-11 18:23:44 +0000

    add sha512 to packaging script
    git-svn-id: svn://localhost/repos@169 16ffc253-0420-0410-a4db-cfc06cdcecfe

--- sfe/sshpackage.sh
+++ sfe/sshpackage.sh
@@ -1,5 +1,5 @@
-tar cvjf sshsfe-src.tar.bz2 --exclude IARPA_DB --exclude make\* --exclude make\* --exclude .\* ssh-src/include/ ssh-src/dropbear/ ssh-src/src/ ssh-src/sillylib/ ssh-src/md5*
+tar cvjf sshsfe-src.tar.bz2 --exclude IARPA_DB --exclude make\* --exclude make\* --exclude .\* ssh-src/include/ ssh-src/dropbear/ ssh-src/src/ ssh-src/sillylib/src/ ssh-src/md5* ssh-src/sha*
 tar cvf sshsfe-bin-`arch`.tar -C ~/sfe/dropbear dropbear dbclient
-tar rvf sshsfe-bin-`arch`.tar -C ssh-src/ $(cd ssh-src/ ; echo md5* )
+tar rvf sshsfe-bin-`arch`.tar -C ssh-src/ $(cd ssh-src/ ; echo md5* sha* )
 rm sshsfe-bin-`arch`.tar.bz2
 bzip2 sshsfe-bin-`arch`.tar
