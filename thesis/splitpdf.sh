64c0d2505192ab789dbffadbe0181f0e7c26b3ab
guest <guest@16ffc253-0420-0410-a4db-cfc06cdcecfe>
2010-05-28 03:45:09 +0000

    useful script for making booklets
    git-svn-id: svn://localhost/repos@177 16ffc253-0420-0410-a4db-cfc06cdcecfe

---
+++ thesis/splitpdf.sh
@@ -0,0 +1,31 @@
+#set -x
+
+bookn=32
+in="$1"
+
+#layout="${bookn}r,1r,2l,$((bookn-1))l"
+layout="${bookn}r,1r,$((bookn-1))l,2l"
+for ((i=3; i<=bookn/2; i=i+2)) ; do
+#  layout=$layout,$((bookn-i+1))r,${i}r,$((i+1))l,$((bookn-i))l
+  layout=$layout,$((bookn-i+1))r,${i}r,$((bookn-i))l,$((i+1))l
+done
+
+echo $layout
+
+acro="acroread"
+pages=$(pdfInfo "$in" | grep "^Page count:" | sed -e 's/Page count: //')
+for ((i=1; i<=pages; i+=bookn)) ; do
+  j=$(( i + bookn - 1))
+  if (( j > pages )) ; then
+    j=$pages
+  fi
+  ii=$(printf %04d $i)
+  out="${in%.pdf}"-$ii.pdf
+  pdfSplit -pages $i-$j "$in"
+  mv "${in%.pdf}"-x.pdf "$out" 
+#  acro="$acro $out"
+
+  pdfImpose -verbose -dim 1x2 -layout $layout "$out"
+done
+
+#eval $acro &
